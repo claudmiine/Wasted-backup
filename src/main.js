ca76c68bf11e51c7114aa8e577c003b0150327bf
claudmiine <wojcieska.klaudia@gmail.com>
2023-04-10 17:13:03 +0100

    first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import { initializeApp } from "firebase/app";
+import router from './router'
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import './assets/main.css'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
